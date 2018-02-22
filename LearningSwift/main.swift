d5cf5c69639c35064a91790a29d3798faf377dfd
Bharg Sharma <bharg.sharma@rea-group.com>
2018-02-22 13:42:52 +1100

    Initial Commit

---
+++ LearningSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LearningSwift
+//
+//  Created by Bharg Sharma on 22/2/18.
+//  Copyright © 2018 Bharg Sharma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
